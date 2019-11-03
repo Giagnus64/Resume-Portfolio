fcfec48ad131846686db5b91c239d02c0dccba3e
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-11-03 12:43:29 -0500

    added edited ruby/rails svg icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-
-import { Link } from 'gatsby';
+import rails from '../assets/images/Rails.svg'
+import ruby from '../assets/images/Ruby.svg'
+//import { Link } from 'gatsby';
 import Sidebar from '../components/Sidebar';
 import config from '../../config';
 const IndexPage = () => (
@@ -19,14 +20,10 @@ const IndexPage = () => (
             <span className="text-primary">{config.lastName}</span>
           </h1>
           <div className="subheading mb-5">
-            {config.address} · {config.phone} ·
             <a href={`mailto:${config.email}`}>{config.email}</a>
           </div>
           <p className="lead mb-5">
-            I am experienced in leveraging agile frameworks to provide a robust
-            synopsis for high level overviews. Iterative approaches to corporate
-            strategy foster collaborative thinking to further the overall value
-            proposition.
+            Full stack web developer with experience in Rails, JavaScript, and React. My music production background strengthens my ability to focus on details while being mindful of the big picture. Impressed by the utility of audio/visual technologies in project creation, I transitioned into web development to build software that impacts global issues.
           </p>
           <div className="social-icons">
             {config.socialLinks.map(social => {
@@ -45,78 +42,119 @@ const IndexPage = () => (
 
       <section
         className="resume-section p-3 p-lg-5 d-flex justify-content-center"
-        id="experience"
+        id="projects"
       >
         <div className="w-100">
-          <h2 className="mb-5">Experience</h2>
+          <h2 className="mb-5">Projects</h2>
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">Senior Web Developer</h3>
-              <div className="subheading mb-3">Intelitec Solutions</div>
-              <p>
-                Bring to the table win-win survival strategies to ensure
-                proactive domination. At the end of the day, going forward, a
-                new normal that has evolved from generation X is on the runway
-                heading towards a streamlined cloud solution. User generated
-                content in real-time will have multiple touchpoints for
-                offshoring.
-              </p>
+              <h3 className="mb-0">Mangia</h3>
+              <div className="subheading mb-3">A web application designed to help users plan and keep track of meals and recipes.</div>
+              <ul>
+                <li>Developed a Rails API backend with custom routes and search functionality</li>
+                <li>Generated UI using React, Bulma CSS framework, and Redux</li>
+                <li>Utilized Edamam API to search recipes by ingredient</li>
+                <li>Added image upload functionality using Google’s Firebase services</li>
+              </ul>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">March 2013 - Present</span>
+              <span className="text-primary"><a href="https://github.com/Giagnus64/Mangia-API">Github</a> | <a href="https://mangia-client.herokuapp.com/login">Live</a></span>
             </div>
           </div>
-
+          
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">Web Developer</h3>
-              <div className="subheading mb-3">Intelitec Solutions</div>
-              <p>
-                Capitalize on low hanging fruit to identify a ballpark value
-                added activity to beta test. Override the digital divide with
-                additional clickthroughs from DevOps. Nanotechnology immersion
-                along the information highway will close the loop on focusing
-                solely on the bottom line.
-              </p>
+              <h3 className="mb-0">Bedfellows</h3>
+              <div className="subheading mb-3">A webapp that connects users and allows them to manage their relationships with each other.</div>
+              <ul>
+                <li>Configured a Rails API backend for ease of use and scalability</li>
+                <li>Designed client interface using React with Semantic UI styling framework </li>
+                <li>Implemented Auth using BCrypt for Password Storage and JWT for Token Creation</li>
+                <li>Utilized React Router for Client-Side Routing</li>
+              </ul>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">December 2011 - March 2013</span>
+              <span className="text-primary"><a href="https://github.com/Giagnus64/Bedfellows-Front-End">Github</a> | <a href="https://bedfellows-client.herokuapp.com/">Live</a></span>
             </div>
           </div>
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">Junior Web Designer</h3>
-              <div className="subheading mb-3">Shout! Media Productions</div>
-              <p>
-                Podcasting operational change management inside of workflows to
-                establish a framework. Taking seamless key performance
-                indicators offline to maximise the long tail. Keeping your eye
-                on the ball while performing a deep dive on the start-up
-                mentality to derive convergence on cross-platform integration.
-              </p>
+              <h3 className="mb-0">Chitter-Chatter</h3>
+              <div className="subheading mb-3">A messaging app that plays sounds and animations with every message.</div>
+              <ul>
+                <li>Programmed an API with Rails for database management and quick setup</li>
+                <li>Designed UI using custom CSS and JS</li>
+                <li>Scripted animations using vector graphics library Paper.js</li>
+                <li>Linked sounds to keypresses using JavaScript music library Howler.js</li>
+              </ul>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">July 2010 - December 2011</span>
+              <span className="text-primary"><a href="https://github.com/Giagnus64/Chitter-Chatter-Front-End">Github</a> | <a href="#">Live</a></span>
             </div>
           </div>
+        </div>
+      </section>
 
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
-            <div className="resume-content">
-              <h3 className="mb-0">Web Design Intern</h3>
-              <div className="subheading mb-3">Shout! Media Productions</div>
-              <p>
-                Collaboratively administrate empowered markets via plug-and-play
-                networks. Dynamically procrastinate B2C users after installed
-                base benefits. Dramatically visualize customer directed
-                convergence without revolutionary ROI.
-              </p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">September 2008 - June 2010</span>
-            </div>
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="skills"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Skills</h2>
+
+          <div className="subheading mb-3">
+            Programming Languages &amp; Tools
           </div>
+          <ul className="list-inline dev-icons">
+            <li className="list-inline-item">
+              <i className="fab fa-html5"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-css3-alt"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-js-square"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-react"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-node-js"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-npm"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab rails"><object type="image/svg+xml" data={rails} className="fab"></object></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab rails"><object type="image/svg+xml" data={ruby} className="fab rails"></object></i>
+            </li>
+          </ul>
+
+          <div className="subheading mb-3">Workflow</div>
+          <ul className="fa-ul mb-0">
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Mobile-First, Responsive Design
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Cross Browser Testing &amp; Debugging
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Cross Functional Teams
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Agile Development &amp; Scrum
+            </li>
+          </ul>
         </div>
       </section>
 
@@ -151,79 +189,6 @@ const IndexPage = () => (
               <span className="text-primary">August 2002 - May 2006</span>
             </div>
           </div>
-        </div>
-      </section>
-
-      <hr className="m-0" />
-
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="skills"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Skills</h2>
-
-          <div className="subheading mb-3">
-            Programming Languages &amp; Tools
-          </div>
-          <ul className="list-inline dev-icons">
-            <li className="list-inline-item">
-              <i className="fab fa-html5"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-css3-alt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-js-square"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-angular"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-react"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-node-js"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-sass"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-less"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-wordpress"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-gulp"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-grunt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-npm"></i>
-            </li>
-          </ul>
-
-          <div className="subheading mb-3">Workflow</div>
-          <ul className="fa-ul mb-0">
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Mobile-First, Responsive Design
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Cross Browser Testing &amp; Debugging
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Cross Functional Teams
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Agile Development &amp; Scrum
-            </li>
-          </ul>
         </div>
       </section>
 
